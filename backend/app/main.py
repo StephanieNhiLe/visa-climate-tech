bb8728606518677886e503f856c3425ff4196bb9
Stephanie Nhi Le <ledaquynhnhi@gmail.com>
2024-06-17 11:04:44 -0500

    fix routes & link flask-react sucessfully

--- backend/app/main.py
+++ backend/app/main.py
@@ -15,4 +15,4 @@ def get_data():
     })
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True)
--- src/App.js
+++ src/App.js
@@ -1,30 +1,15 @@
 import './App.css';
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from 'react'; 
 
 function App() {
   const [message, setMessage] = useState('Loading...');
 
   useEffect(() => {
-    const fetchAPI = async () => {
-      try {
-        const response = await axios.get('http://localhost:5000/api/messages');
-        console.log('Connection is ' + response.data.message + '!');
-        setMessage(response.data.message);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-  
-    fetchAPI();
+    fetch('http://127.0.0.1:5000/api/messages')
+      .then(response => response.json())
+      .then(data => setMessage(data.message))
+      .catch(error => console.error('Error fetching data:', error));
   }, []);
-
-  // useEffect(() => {
-  //   fetch('http://localhost:5000/api/messages')
-  //     .then(response => response.json())
-  //     .then(data => setMessage(data.message))
-  //     .catch(error => console.error('Error fetching data:', error));
-  // }, []);
   
   return (
     <div className="App">
