47a8755d8e0422ff1298faab5a3c75e8bf0932dd
Stephanie Nhi Le <ledaquynhnhi@gmail.com>
2024-06-17 10:24:51 -0500

    attempted with williams work

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,18 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/api/*": {"origins": "*"}})
+CORS(app, origins='*', allow_headers=[
+    "Content-Type", "Authorization",
+    "Access-Control-Allow-Credentials"],
+    supports_credentials=True)
 
-@app.route('/api/data')
+@app.route('/api/messages', methods=['GET'])
 def get_data():
-    return {"message": "Hello, Visa Hack Team!"}
+    # return {"message": "Hello, Visa Hack Team!"}
+    return jsonify({
+        'message': "successful!"
+    })
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, port=5000)
--- src/App.js
+++ src/App.js
@@ -1,32 +1,35 @@
 import './App.css';
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
-  const [data, setData] = useState('Loading...');
+  const [message, setMessage] = useState('Loading...');
 
   useEffect(() => {
-    fetch('http://localhost:5000/api/data', {
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
+    const fetchAPI = async () => {
+      try {
+        const response = await axios.get('http://localhost:5000/api/messages');
+        console.log('Connection is ' + response.data.message + '!');
+        setMessage(response.data.message);
+      } catch (error) {
+        console.error('Error fetching data:', error);
       }
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        return response.json();
-      })
-      .then((data) => setData(data.message))
-      .catch((error) => console.error('Error fetching data:', error));
+    };
+  
+    fetchAPI();
   }, []);
-  
-  
 
+  // useEffect(() => {
+  //   fetch('http://localhost:5000/api/messages')
+  //     .then(response => response.json())
+  //     .then(data => setMessage(data.message))
+  //     .catch(error => console.error('Error fetching data:', error));
+  // }, []);
+  
   return (
     <div className="App">
       <header className="App-header">
-        <p>The message is {data}</p>
+        <p>The message is {message}</p>
       </header>
     </div>
   );
