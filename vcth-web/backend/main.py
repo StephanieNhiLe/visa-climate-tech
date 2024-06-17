5a79ae315f7e611bc6a582f3c16f4766fb2c8e74
William Ng <wng003@citymail.cuny.edu>
2024-06-17 07:01:12 -0400

    Resolves #3 - Update + Fix Frontend to Backend

---
+++ vcth-web/backend/main.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+cors = CORS(app, origins='*', allow_headers=[
+    "Content-Type", "Authorization  ", "Access-Control-Allow-Credentials"], supports_credentials=True)     # CORS
+@app.route("/api/messages", methods=["GET"])  
+def get_message():
+    return jsonify(
+        {
+            "message": "successful!"
+        }
+    )
+
+# @app.route("/api/users", methods=["GET"])
+# def get_users():
+#     return jsonify(
+#         {
+#            "users": [
+#                 'Alex', 'Bob', 'Charlie'
+#            ]     
+#         }
+#     )
+
+if __name__ == "__main__":
+    app.run(debug=True,port=8080)
--- vcth-web/src/App.jsx
+++ vcth-web/src/App.jsx
@@ -1,19 +1,68 @@
-import React from 'react';
-import AOS from 'aos';
+import { useState, useEffect } from 'react';
+// import AOS from 'aos'; // react aos scroll animation
+import './App.css';
+import axios from 'axios';
 import NavBar from './components/NavBar';
 
-const App = () => {
-  React.useEffect(() => {
-    AOS.init({
-        duration: 2000,
-        easing: 'ease-in-out',
-        once: true,
-      });
+function App(){
+  const [message, setMessage] = useState('Loading.....');
+  // useEffect(() => {
+  //   AOS.init({
+  //       duration: 2000,
+  //       easing: 'ease-in-out',
+  //       once: true,
+  //     });
+  // }, []);
+
+  const fetchAPI = async () => {
+    try {
+      const response = await axios.get('http://localhost:8080/api/messages');
+      console.log('Connection is ' + response.data.message + '!');
+      setMessage(response.data.message);
+    }
+    catch (error) {
+      console.error('Error fetching data:',error);
+    }
+  };
+
+  useEffect(() => {
+    fetchAPI();
   }, []);
+
   return (
     <div className ="overflow-hidden">
+      <h1 className='Title'>
       Visa Climate Hack "Solution" Website
+      </h1>
+      <br>
+      </br>
       <NavBar />
+      <br>
+      </br>
+        <div className="App"><header className="App-header">
+          <p>The Connection is {message}</p>
+        </header>
+        </div>
+        <br>
+      </br>
+          <div className="container">
+            <div className="row">
+              <div className="col-md-6">
+                <h2>Our Mission</h2>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae velit fugit accusantium vel earum illum, quasi, quia distinctio temporibus necessitatibus totam. Consequuntur tenetur, veniam illum veritatis placeat aspernatur quo molestiae!
+                </p>
+              </div>
+              <br>
+              </br>
+              <div className="col-md-6">
+                <h2>Our Partners</h2>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae velit fugit accusantium vel earum illum, quasi, quia distinctio temporibus necessitatibus totam. Consequuntur tenetur, veniam illum veritatis placeat aspernatur quo molestiae!
+                </p>
+              </div>
+            </div>
+      </div>
     </div>
   );
 };
