04270b68146003e1f7a5d979d63c5d3ab51db0e7
William Ng <wng003@citymail.cuny.edu>
2024-06-19 04:10:43 -0400

    Update + DB implementation

--- vcth-web/backend/main.py
+++ vcth-web/backend/main.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template, request, jsonify
+from flask_restful import Api, Resource, reqparse
+from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+db = SQLAlchemy(app) # Database
+api = Api(app)  # API
+
 cors = CORS(app, origins='*', allow_headers=[
     "Content-Type", "Authorization  ", "Access-Control-Allow-Credentials"], supports_credentials=True)     # CORS
-@app.route("/api/messages", methods=["GET"])  
+@app.route("http://127.0.0.1:5000/api/messages", methods=["GET"])  
 def get_message():
     return jsonify(
         {
@@ -13,4 +19,4 @@ def get_message():
     )
 
 if __name__ == "__main__":
-    app.run(debug=True,port=8080)
+    app.run(debug=True,port=5000)
